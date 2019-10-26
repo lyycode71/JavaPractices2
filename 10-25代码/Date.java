49b006a8280eda6f498ba7cd2e88ab272f8fa97f
lyycode71 <1823490029@qq.com>
2019-10-26 14:48:01 +0800

    java

---
+++ 10-25代码/Date.java
@@ -0,0 +1,16 @@
+public class Date{
+	public static void main(String[] args){
+		short minShort=Short.MIN_VALUE;
+		short maxShort=Short.MAX_VALUE;
+		System.out.println(minShort);
+		System.out.println(maxShort);
+		long minLong=Long.MIN_VALUE;
+		long maxLong=Long.MAX_VALUE;
+		System.out.println(minLong);
+		System.out.println(maxLong);
+		long a=010L;
+		System.out.println(a);
+		int b=0x2C;
+		System.out.println(b);
+	}
+}

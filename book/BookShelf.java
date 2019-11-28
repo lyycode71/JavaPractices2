6a6a7cbf49e03b117cab4965414a9f477e6ea724
lyycode71 <1823490029@qq.com>
2019-11-28 23:02:34 +0800

    java

---
+++ book/BookShelf.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class BookShelf {
+    private static final BookShelf instance=new BookShelf();
+    private BookShelf(){}
+    public static BookShelf getBookShelf(){
+        return instance;
+    }
+    Book[] array=new Book[100];
+    int size;
+    public void add(Book book){
+        array[size++]=book;
+    }
+    public Book[] getBooks(){
+        return Arrays.copyOf(array,size);
+    }
+}
